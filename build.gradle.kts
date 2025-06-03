9695216bc5037c5e91b98507f5f175ac199f241c
Niklas Widmann <nik@somethingcatchy.net>
2025-06-03 21:42:22 +0200

    set version in CI using env variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,5 @@
-val mod_version: String by extra
-val mc_version: String by extra
-
 plugins {
     id("com.possible-triangle.gradle") version ("0.2.12")
-}
-
-mod {
-    version = "$mc_version-$mod_version"
 }
 
 subprojects {
