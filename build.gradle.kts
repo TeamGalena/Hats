f444d59d9d7ce35f46808a5c9aa3c285e30722b9
Niklas Widmann <nik@somethingcatchy.net>
2025-08-04 10:38:23 +0200

    add red youtube hat

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.possible-triangle.gradle") version ("0.2.13")
+    id("com.possible-triangle.gradle") version ("0.2.17")
 }
 
 subprojects {
@@ -14,16 +14,9 @@ subprojects {
         }
     }
 
-    tasks.withType<Jar> {
-        exclude("**/*.bbmodel")
-    }
-
     enablePublishing {
         name = "hats-${project.name}"
-
-        repositories {
-            if (env.isCI) nexus()
-        }
+        nexus()
     }
 }
 
